a84b1b3d92557c72a9f8deeaec12c7b21c199150
Jafet Bjarkar <jafetbjarkar@gmail.com>
2014-12-18 11:36:32 +0000

    Activate OWL sliders

--- content-album.php
+++ content-album.php
@@ -15,6 +15,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php the_field('playlist'); ?>
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 
--- js/extra.js
+++ js/extra.js
@@ -1,3 +1,19 @@
 jQuery(function( $ ) {
-  
+  $("#content").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      lazyLoad: true,
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+
 });
