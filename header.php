8abf6550bfcd97e64e889515eb705aa561bffb33
Jafet Bjarkar <jafetbjarkar@gmail.com>
2015-03-01 00:06:42 +0000

    Media queries added

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=640 maximum-scale=1">
+	<meta name="viewport" content="width=500 maximum-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
